64e04e45ecadf865bfdd83aafd7e8c8095e18d2d
musthafa1122 <mhdmusthafa500@gmail.com>
2024-10-02 20:40:25 +0200

    w

---
+++ starter/starter.component.ts
@@ -0,0 +1,26 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MaterialModule } from '../../material.module';
+import {AppRecentTransactionsComponent} from "../../components/recent-transactions/recent-transactions.component";
+import {AppSalesOverviewComponent} from "../../components/sales-overview/sales-overview.component";
+import {AppYearlyBreakupComponent} from "../../components/yearly-breakup/yearly-breakup.component";
+import {AppMonthlyEarningsComponent} from "../../components/monthly-earnings/monthly-earnings.component";
+import {AppProductPerformanceComponent} from "../../components/product-performance/product-performance.component";
+import {AppBlogCardsComponent} from "../../components/blog-card/blog-card.component";
+
+
+@Component({
+  selector: 'app-starter',
+  standalone: true,
+  imports: [
+    MaterialModule,
+    AppSalesOverviewComponent,
+    AppYearlyBreakupComponent,
+    AppMonthlyEarningsComponent,
+    AppRecentTransactionsComponent,
+    AppProductPerformanceComponent,
+    AppBlogCardsComponent
+  ],
+  templateUrl: './starter.component.html',
+  encapsulation: ViewEncapsulation.None,
+})
+export class StarterComponent { }
