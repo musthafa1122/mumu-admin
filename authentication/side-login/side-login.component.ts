6a35e8495ca833759c86b22ee2932ff5b5d0f96b
musthafa1122 <mhdmusthafa500@gmail.com>
2024-10-04 19:13:19 +0200

    add auth0

--- authentication/side-login/side-login.component.ts
+++ authentication/side-login/side-login.component.ts
@@ -1,14 +1,9 @@
-import { Component } from '@angular/core';
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { Router, RouterModule } from '@angular/router';
-import { MaterialModule } from '../../../material.module';
-import { MatButtonModule } from '@angular/material/button';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators,} from '@angular/forms';
+import {Router, RouterModule} from '@angular/router';
+import {MaterialModule} from '../../../material.module';
+import {MatButtonModule} from '@angular/material/button';
+import {AuthService} from "@auth0/auth0-angular";
 
 @Component({
   selector: 'app-side-login',
@@ -23,19 +18,23 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './side-login.component.html',
 })
 export class AppSideLoginComponent {
-  constructor(private router: Router) {}
-
   form = new FormGroup({
     uname: new FormControl('', [Validators.required, Validators.minLength(6)]),
     password: new FormControl('', [Validators.required]),
   });
+
+  constructor(private router: Router, private authService: AuthService,) {
+  }
 
   get f() {
     return this.form.controls;
   }
 
   submit() {
-    // console.log(this.form.value);
-    this.router.navigate(['/']);
+
+    this.authService.loginWithPopup().subscribe(data => {
+      this.router.navigate(['/dashboard']);
+    })
+    // this.router.navigate(['/']);
   }
 }
