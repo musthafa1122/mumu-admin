af24cba65ed543e0d6831abc0758383bd639431d
musthafa1122 <mhdmusthafa500@gmail.com>
2024-10-06 04:09:16 +0200

    add auth0

--- available-workers/available-workers.component.ts
+++ available-workers/available-workers.component.ts
@@ -1,7 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MaterialModule} from "../../material.module";
 import {TablerIconsModule} from "angular-tabler-icons";
 import {CommonModule} from "@angular/common";
+
+interface UserCard {
+  id: number;
+  imgSrc: string;
+  name: string;
+  profession: string;
+  address: string;
+  phone: string;
+  serviceType: 'driver' | 'errands' | 'bystander';
+}
 
 @Component({
   selector: 'app-available-workers',
@@ -10,8 +20,12 @@ import {CommonModule} from "@angular/common";
   templateUrl: './available-workers.component.html',
   styleUrl: './available-workers.component.scss'
 })
-export class AvailableWorkersComponent {
-  public userCards = [
+
+export class AvailableWorkersComponent implements OnInit {
+  public driversCount = 0
+  public errandsCount = 0
+  public bystandersCount = 0
+  public userCards: UserCard[] = [
     {
       id: 1,
       imgSrc: 'assets/images/profile/user-2.jpg',
@@ -19,14 +33,16 @@ export class AvailableWorkersComponent {
       profession: 'Driver',
       address: '795 Folsom Ave, NY, 10003',
       phone: '(123) 456-7890',
+      serviceType: 'driver',
     },
     {
       id: 2,
       imgSrc: 'assets/images/profile/user-4.jpg',
       name: 'George Johnson',
       profession: 'Errand Runner',
-      address: '36 W 138th St,  NY, 10037',
+      address: '36 W 138th St, NY, 10037',
       phone: '(212) 234-0783',
+      serviceType: 'errands',
     },
     {
       id: 3,
@@ -35,6 +51,7 @@ export class AvailableWorkersComponent {
       profession: 'Bystander for Patient',
       address: '123 Main St, San Francisco, CA, 94105',
       phone: '(415) 987-6543',
+      serviceType: 'bystander',
     },
     {
       id: 4,
@@ -43,6 +60,7 @@ export class AvailableWorkersComponent {
       profession: 'Driver',
       address: '1725 Slough Ave, Scranton, PA, 18505',
       phone: '(570) 555-0000',
+      serviceType: 'driver',
     },
     {
       id: 5,
@@ -51,6 +69,7 @@ export class AvailableWorkersComponent {
       profession: 'Errand Runner',
       address: '1855 Beet Farm Rd, Scranton, PA, 18505',
       phone: '(570) 555-5555',
+      serviceType: 'errands',
     },
     {
       id: 6,
@@ -59,443 +78,63 @@ export class AvailableWorkersComponent {
       profession: 'Bystander for Elderly',
       address: '1725 Slough Ave, Scranton, PA, 18505',
       phone: '(570) 555-1212',
+      serviceType: 'bystander',
     },
     {
       id: 7,
       imgSrc: 'assets/images/profile/user-2.jpg',
       name: 'Jim Halpert',
       profession: 'Driver',
-      address: '456 Paper St, Scranton, PA, 18505',
-      phone: '(570) 555-1234',
+      address: '1725 Slough Ave, Scranton, PA, 18505',
+      phone: '(570) 555-2222',
+      serviceType: 'driver',
     },
     {
       id: 8,
       imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Kelly Kapoor',
+      name: 'Stanley Hudson',
       profession: 'Errand Runner',
-      address: '789 Fashion Ave, New York, NY, 10001',
-      phone: '(212) 555-7890',
+      address: '1725 Slough Ave, Scranton, PA, 18505',
+      phone: '(570) 555-3333',
+      serviceType: 'errands',
     },
     {
       id: 9,
       imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Ryan Howard',
-      profession: 'Driver',
-      address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-6789',
-    },
-    {
-      id: 10,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Stanley Hudson',
-      profession: 'Bystander for Patient',
-      address: '500 Paper St, Scranton, PA, 18505',
-      phone: '(570) 555-2345',
-    },
-    {
-      id: 11,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Angela Martin',
-      profession: 'Errand Runner',
-      address: '1855 Accountant St, Scranton, PA, 18505',
-      phone: '(570) 555-9876',
-    },
-    {
-      id: 12,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Oscar Martinez',
-      profession: 'Driver',
-      address: '500 Accountant St, Scranton, PA, 18505',
-      phone: '(570) 555-3456',
-    },
-    {
-      id: 1,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Johnathan Doe',
-      profession: 'Driver',
-      address: '795 Folsom Ave, NY, 10003',
-      phone: '(123) 456-7890',
-    },
-    {
-      id: 2,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'George Johnson',
-      profession: 'Errand Runner',
-      address: '36 W 138th St,  NY, 10037',
-      phone: '(212) 234-0783',
-    },
-    {
-      id: 3,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Sarah Connor',
-      profession: 'Bystander for Patient',
-      address: '123 Main St, San Francisco, CA, 94105',
-      phone: '(415) 987-6543',
-    },
-    {
-      id: 4,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Michael Scott',
-      profession: 'Driver',
-      address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-0000',
-    },
-    {
-      id: 5,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Dwight Schrute',
-      profession: 'Errand Runner',
-      address: '1855 Beet Farm Rd, Scranton, PA, 18505',
-      phone: '(570) 555-5555',
-    },
-    {
-      id: 6,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Pam Beesly',
+      name: 'Phyllis Vance',
       profession: 'Bystander for Elderly',
       address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-1212',
-    },
-    {
-      id: 7,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Jim Halpert',
-      profession: 'Driver',
-      address: '456 Paper St, Scranton, PA, 18505',
-      phone: '(570) 555-1234',
-    },
-    {
-      id: 8,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Kelly Kapoor',
-      profession: 'Errand Runner',
-      address: '789 Fashion Ave, New York, NY, 10001',
-      phone: '(212) 555-7890',
-    },
-    {
-      id: 9,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Ryan Howard',
-      profession: 'Driver',
-      address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-6789',
+      phone: '(570) 555-4444',
+      serviceType: 'bystander',
     },
     {
       id: 10,
       imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Stanley Hudson',
-      profession: 'Bystander for Patient',
-      address: '500 Paper St, Scranton, PA, 18505',
-      phone: '(570) 555-2345',
+      name: 'Angela Martin',
+      profession: 'Driver',
+      address: '1725 Slough Ave, Scranton, PA, 18505',
+      phone: '(570) 555-5550',
+      serviceType: 'driver',
     },
     {
       id: 11,
       imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Angela Martin',
+      name: 'Kevin Malone',
       profession: 'Errand Runner',
-      address: '1855 Accountant St, Scranton, PA, 18505',
-      phone: '(570) 555-9876',
-    },
-    {
-      id: 12,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Oscar Martinez',
-      profession: 'Driver',
-      address: '500 Accountant St, Scranton, PA, 18505',
-      phone: '(570) 555-3456',
-    },
-    {
-      id: 1,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Johnathan Doe',
-      profession: 'Driver',
-      address: '795 Folsom Ave, NY, 10003',
-      phone: '(123) 456-7890',
-    },
-    {
-      id: 2,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'George Johnson',
-      profession: 'Errand Runner',
-      address: '36 W 138th St,  NY, 10037',
-      phone: '(212) 234-0783',
-    },
-    {
-      id: 3,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Sarah Connor',
-      profession: 'Bystander for Patient',
-      address: '123 Main St, San Francisco, CA, 94105',
-      phone: '(415) 987-6543',
-    },
-    {
-      id: 4,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Michael Scott',
-      profession: 'Driver',
       address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-0000',
-    },
-    {
-      id: 5,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Dwight Schrute',
-      profession: 'Errand Runner',
-      address: '1855 Beet Farm Rd, Scranton, PA, 18505',
-      phone: '(570) 555-5555',
-    },
-    {
-      id: 6,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Pam Beesly',
-      profession: 'Bystander for Elderly',
-      address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-1212',
-    },
-    {
-      id: 7,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Jim Halpert',
-      profession: 'Driver',
-      address: '456 Paper St, Scranton, PA, 18505',
-      phone: '(570) 555-1234',
-    },
-    {
-      id: 8,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Kelly Kapoor',
-      profession: 'Errand Runner',
-      address: '789 Fashion Ave, New York, NY, 10001',
-      phone: '(212) 555-7890',
-    },
-    {
-      id: 9,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Ryan Howard',
-      profession: 'Driver',
-      address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-6789',
-    },
-    {
-      id: 10,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Stanley Hudson',
-      profession: 'Bystander for Patient',
-      address: '500 Paper St, Scranton, PA, 18505',
-      phone: '(570) 555-2345',
-    },
-    {
-      id: 11,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Angela Martin',
-      profession: 'Errand Runner',
-      address: '1855 Accountant St, Scranton, PA, 18505',
-      phone: '(570) 555-9876',
-    },
-    {
-      id: 12,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Oscar Martinez',
-      profession: 'Driver',
-      address: '500 Accountant St, Scranton, PA, 18505',
-      phone: '(570) 555-3456',
-    },
-    {
-      id: 1,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Johnathan Doe',
-      profession: 'Driver',
-      address: '795 Folsom Ave, NY, 10003',
-      phone: '(123) 456-7890',
-    },
-    {
-      id: 2,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'George Johnson',
-      profession: 'Errand Runner',
-      address: '36 W 138th St,  NY, 10037',
-      phone: '(212) 234-0783',
-    },
-    {
-      id: 3,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Sarah Connor',
-      profession: 'Bystander for Patient',
-      address: '123 Main St, San Francisco, CA, 94105',
-      phone: '(415) 987-6543',
-    },
-    {
-      id: 4,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Michael Scott',
-      profession: 'Driver',
-      address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-0000',
-    },
-    {
-      id: 5,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Dwight Schrute',
-      profession: 'Errand Runner',
-      address: '1855 Beet Farm Rd, Scranton, PA, 18505',
-      phone: '(570) 555-5555',
-    },
-    {
-      id: 6,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Pam Beesly',
-      profession: 'Bystander for Elderly',
-      address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-1212',
-    },
-    {
-      id: 7,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Jim Halpert',
-      profession: 'Driver',
-      address: '456 Paper St, Scranton, PA, 18505',
-      phone: '(570) 555-1234',
-    },
-    {
-      id: 8,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Kelly Kapoor',
-      profession: 'Errand Runner',
-      address: '789 Fashion Ave, New York, NY, 10001',
-      phone: '(212) 555-7890',
-    },
-    {
-      id: 9,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Ryan Howard',
-      profession: 'Driver',
-      address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-6789',
-    },
-    {
-      id: 10,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Stanley Hudson',
-      profession: 'Bystander for Patient',
-      address: '500 Paper St, Scranton, PA, 18505',
-      phone: '(570) 555-2345',
-    },
-    {
-      id: 11,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Angela Martin',
-      profession: 'Errand Runner',
-      address: '1855 Accountant St, Scranton, PA, 18505',
-      phone: '(570) 555-9876',
-    },
-    {
-      id: 12,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Oscar Martinez',
-      profession: 'Driver',
-      address: '500 Accountant St, Scranton, PA, 18505',
-      phone: '(570) 555-3456',
-    },
-    {
-      id: 1,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Johnathan Doe',
-      profession: 'Driver',
-      address: '795 Folsom Ave, NY, 10003',
-      phone: '(123) 456-7890',
-    },
-    {
-      id: 2,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'George Johnson',
-      profession: 'Errand Runner',
-      address: '36 W 138th St,  NY, 10037',
-      phone: '(212) 234-0783',
-    },
-    {
-      id: 3,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Sarah Connor',
-      profession: 'Bystander for Patient',
-      address: '123 Main St, San Francisco, CA, 94105',
-      phone: '(415) 987-6543',
-    },
-    {
-      id: 4,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Michael Scott',
-      profession: 'Driver',
-      address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-0000',
-    },
-    {
-      id: 5,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Dwight Schrute',
-      profession: 'Errand Runner',
-      address: '1855 Beet Farm Rd, Scranton, PA, 18505',
-      phone: '(570) 555-5555',
-    },
-    {
-      id: 6,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Pam Beesly',
-      profession: 'Bystander for Elderly',
-      address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-1212',
-    },
-    {
-      id: 7,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Jim Halpert',
-      profession: 'Driver',
-      address: '456 Paper St, Scranton, PA, 18505',
-      phone: '(570) 555-1234',
-    },
-    {
-      id: 8,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Kelly Kapoor',
-      profession: 'Errand Runner',
-      address: '789 Fashion Ave, New York, NY, 10001',
-      phone: '(212) 555-7890',
-    },
-    {
-      id: 9,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Ryan Howard',
-      profession: 'Driver',
-      address: '1725 Slough Ave, Scranton, PA, 18505',
-      phone: '(570) 555-6789',
-    },
-    {
-      id: 10,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Stanley Hudson',
-      profession: 'Bystander for Patient',
-      address: '500 Paper St, Scranton, PA, 18505',
-      phone: '(570) 555-2345',
-    },
-    {
-      id: 11,
-      imgSrc: 'assets/images/profile/user-2.jpg',
-      name: 'Angela Martin',
-      profession: 'Errand Runner',
-      address: '1855 Accountant St, Scranton, PA, 18505',
-      phone: '(570) 555-9876',
-    },
-    {
-      id: 12,
-      imgSrc: 'assets/images/profile/user-4.jpg',
-      name: 'Oscar Martinez',
-      profession: 'Driver',
-      address: '500 Accountant St, Scranton, PA, 18505',
-      phone: '(570) 555-3456',
+      phone: '(570) 555-6666',
+      serviceType: 'errands',
     },
   ];
+  filteredCards!: UserCard[];
 
+  ngOnInit(): void {
+    this.filteredCards = this.userCards;
+    this.bystandersCount = this.userCards.filter(data => data.serviceType === 'bystander').length;
+    this.driversCount = this.userCards.filter(data => data.serviceType === 'driver').length;
+    this.errandsCount = this.userCards.filter(data => data.serviceType === 'errands').length;
+  }
 
-  public filteredCards = this.userCards; // Array for filtered results
 
   // Function to filter user cards based on the search term
   applyFilter(event: KeyboardEvent) {
@@ -506,5 +145,14 @@ export class AvailableWorkersComponent {
       userCard.profession.toLowerCase().includes(searchTerm) ||
       userCard.address.toLowerCase().includes(searchTerm)
     );
+  }
+
+  clickFilter(serviceType: 'driver' | 'errands' | 'bystander' | null) {
+    if (serviceType !== null) {
+      this.filteredCards = this.userCards.filter(card => card.serviceType === serviceType);
+    } else {
+      this.filteredCards = this.userCards;
+    }
+
   }
 }
