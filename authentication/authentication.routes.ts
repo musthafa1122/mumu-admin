a26c07869e8ba557e6c0d4135c2fb5044b281288
musthafa1122 <mhdmusthafa500@gmail.com>
2024-10-13 15:57:38 +0200

    add auth0

--- authentication/authentication.routes.ts
+++ authentication/authentication.routes.ts
@@ -1,7 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-import { AppSideLoginComponent } from './side-login/side-login.component';
-import { AppSideRegisterComponent } from './side-register/side-register.component';
+import {AppSideLoginComponent} from './side-login/side-login.component';
+import {AppSideRegisterComponent} from './side-register/side-register.component';
+import {OtpVerificationComponent} from "../../components/otp-verification/otp-verification.component";
 
 export const AuthenticationRoutes: Routes = [
   {
@@ -15,6 +16,11 @@ export const AuthenticationRoutes: Routes = [
         path: 'register',
         component: AppSideRegisterComponent,
       },
+      {
+        path: 'otp-verification',
+        component: OtpVerificationComponent,
+
+      }
     ],
   },
 ];
