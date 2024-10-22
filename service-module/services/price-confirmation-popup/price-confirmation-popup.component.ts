f146c1ae77d373efffc4c1eba133be3feeb1109e
musthafa1122 <mhdmusthafa500@gmail.com>
2024-10-22 03:12:33 +0200

    refactore

--- service-module/services/price-confirmation-popup/price-confirmation-popup.component.ts
+++ service-module/services/price-confirmation-popup/price-confirmation-popup.component.ts
@@ -1,14 +1,30 @@
 import {Component} from '@angular/core';
 import {MaterialModule} from "../../../../material.module";
 import {NgApexchartsModule} from "ng-apexcharts";
+import {NgIf} from "@angular/common";
+import {Router} from "@angular/router";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-price-confirmation-popup',
   standalone: true,
-  imports: [MaterialModule, NgApexchartsModule],
+  imports: [MaterialModule, NgApexchartsModule, NgIf],
   templateUrl: './price-confirmation-popup.component.html',
   styleUrl: './price-confirmation-popup.component.scss'
 })
 export class PriceConfirmationPopupComponent {
+  showProgress = false;
+  protected readonly onsubmit = onsubmit;
 
+  constructor(private router: Router, private dialog: MatDialog,) {
+  }
+
+  submit() {
+    this.showProgress = true;
+    setTimeout(() => {
+      this.showProgress = false
+      this.router.navigateByUrl("/service-home/history");
+      this.dialog.closeAll()
+    }, 7000);
+  }
 }
